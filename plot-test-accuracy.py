0dbcef07cb013617be18b807222ca61032d06547
davidedomini <davide.domini@unibo.it>
2024-10-21 21:28:10 +0200

    implement plot test accuracy

--- plot-test-accuracy.py
+++ plot-test-accuracy.py
@@ -6,20 +6,26 @@ import matplotlib
 import glob
 
 
-def get_areas(name):
+def parse_areas(name):
     for elem in name.split('_'):
         if 'areas' in elem:
             n_areas = int(elem[-1])
             return n_areas
 
+def parse_sparsity(name):
+    for elem in name.split('_'):
+        if 'sparsity' in elem:
+            sparsity = float(elem.split('-')[-1])
+            return sparsity
 
-def get_data(directory, algorithm):
-    files = glob.glob(directory)
+def get_data(directory, threshold):
+    files = glob.glob(f'{directory}/*_lossThreshold-{threshold}*.csv')
     df = pd.DataFrame(columns=['Test accuracy', 'Areas', 'Algorithm'])
     for f in files:
-        area = get_areas(f)
+        areas = parse_areas(f)
+        sparsity = parse_sparsity(f)
         acc = pd.read_csv(f).iloc[0].mean()
-        df = df._append({'Test accuracy': acc, 'Areas': area, 'Algorithm': algorithm}, ignore_index=True)
+        df = df._append({'Test accuracy': acc, 'Areas': areas, 'Sparsity': sparsity}, ignore_index=True)
     return df
 
 if __name__ == '__main__':
@@ -31,26 +37,22 @@ if __name__ == '__main__':
     matplotlib.rcParams.update({'axes.labelsize': 30})
     matplotlib.rcParams.update({'xtick.labelsize': 25})
     matplotlib.rcParams.update({'ytick.labelsize': 25})
-    plt.rcParams.update({
-    "text.usetex": True
-    })
+    plt.rcParams.update({"text.usetex": True})
     plt.rc('text.latex', preamble=r'\usepackage{amsmath,amssymb,amsfonts}')
-
-
-    data_baseline = get_data('data-test-baseline/*.csv', 'Baseline')
 
     data_self_fl = {}
 
     for th in [20, 40, 80]:
-        d = get_data(f'data-test/*lossThreshold-{th}.0.csv', 'Self-FL')
+        d = get_data(f'data-test', th)
         data_self_fl[th] = d
 
     for th in data_self_fl.keys():
         plt.figure(figsize=(12, 8))
-        data_comparison = pd.concat([data_baseline, data_self_fl[th]])
-        sns.color_palette('colorblind', as_cmap=True)
-        sns.set_palette('colorblind')
-        ax = sns.boxplot(data=data_comparison, x='Areas', y='Test accuracy', hue='Algorithm')
+        data_comparison = data_self_fl[th]
+        # sns.color_palette('viridis', as_cmap=True)
+        # sns.set_palette('viridis')
+        palette = ['#440154', '#31688e', '#35b779', '#fde725']
+        ax = sns.boxplot(data=data_comparison, x='Areas', y='Test accuracy', hue='Sparsity', palette=palette)
         sns.move_legend(ax, 'lower left')
         plt.title(f'$ \sigma = {th}$')
         plt.ylabel('$Accuracy - Test$')
